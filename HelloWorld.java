aff633b39d0dbc6a11ba208fafa6e31f30ad04ec
Avi <aapatel@scu.edu>
2020-02-14 00:29:29 -0800

    Changes to HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public static void main(String[] a){
-        System.out.println("This is test!");
+        System.out.println("This is a test!");
+        System.out.println("Test for edit!");
     }
 }
