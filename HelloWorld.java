6e01fe848a2b75c7a40f037ff404ebaa94793454
Avi <aapatel@scu.edu>
2020-02-14 00:33:28 -0800

    Change added to test comparison

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
     public static void main(String[] a){
         System.out.println("This is a test!");
         System.out.println("Test for edit!");
+        System.out.println("Test for comparison!");
     }
 }
